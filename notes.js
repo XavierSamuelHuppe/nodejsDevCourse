07255f19275b6a2b8fab551be56271e528ea97de
Xavier Samuel Huppé <XavierSamuelHuppe@users.noreply.github.com>
2017-12-25 17:57:56 +0700

    GET /todos and test

--- notes.js
+++ notes.js
@@ -286,6 +286,47 @@ TESTING REST API
         })
         .end(done)
     })
+  //test POST
+  beforeEach((done) => {
+    Todo.remove({}).then(() => done())
+  })
+  describe ('POST /todos', () => {
+    it('should create a new todo', (done) => {
+      var text = "this is some text"
+      request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.text).toBe(text)
+        })
+        .end((err, res) => {
+          if(err) {
+            return done(err)
+          }
+          return Todo.find().then((todos) => {
+            expect(todos.length).toBe(1)
+            expect(todos[0].text).toBe(text)
+            done()
+          }).catch((e) => done(e))
+        })
+    })
+    it('should not create todo with invalid body data', (done) => {
+      request(app)
+        .post('/todos')
+        .send({text:""})
+        .expect(400)
+        .end((err,res) => {
+          if(err) {
+            return done(err)
+          }
+          return Todo.find().then(todos => {
+            expect(todos.length).toBe(0)
+            done()
+          }).catch((e) => done(e))
+        })
+    })
+  })
 
 MONGO DB
   //vocabulary
