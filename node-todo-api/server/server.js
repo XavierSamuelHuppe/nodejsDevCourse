74643fb9aca00ca5ee6b259d10d27c86f4b93f5c
Xavier Samuel Huppé <XavierSamuelHuppe@users.noreply.github.com>
2017-12-26 17:51:21 +0700

    add GET /users/me and auth middleware

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -9,10 +9,15 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 
 app.use(bodyParser.json())
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user)
+})
 
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ["email", "password"])
