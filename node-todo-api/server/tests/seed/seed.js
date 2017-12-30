ddecbc910a6e6e9f2a68412c57d9164826593516
Xavier Samuel Huppé <XavierSamuelHuppe@users.noreply.github.com>
2017-12-30 14:34:08 +0700

    test on /users route

---
+++ node-todo-api/server/tests/seed/seed.js
@@ -0,0 +1,62 @@
+const {ObjectID} = require('mongodb')
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo')
+const {User} = require('./../../models/user')
+
+const userOneId = new ObjectID()
+const userTwoId = new ObjectID()
+const users = [{
+    _id: userOneId,
+    email: 'userOne@example.com',
+    password: 'userOnePass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({
+        _id: userOneId,
+        access: 'auth'
+      }, 'mysecret').toString()
+    }]
+  },
+  {
+    _id: userTwoId,
+    email: 'userTwo@example.com',
+    password: 'userTwoPass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({
+        _id: userTwoId,
+        access: 'auth'
+      }, 'mysecret').toString()
+    }]
+  }
+]
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: "First todo",
+    completed: false
+  },
+  {
+    _id: new ObjectID(),
+    text: "second todo"
+  }
+]
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done())
+}
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save()
+    var userTwo = new User(users[1]).save()
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done())
+}
+
+module.exports = {todos, populateTodos, users, populateUsers}
--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,24 +4,11 @@ const {ObjectID} = require('mongodb')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
+const {User} = require('./../models/user')
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed')
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: "First todo",
-    completed: false
-  },
-  {
-    _id: new ObjectID(),
-    text: "second todo"
-  }
-]
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos)
-  }).then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -179,3 +166,98 @@ describe('PATCH /todos/:id', () => {
       })
   })
 })
+
+describe('GET /users/me', () => {
+  it('should return correct user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString())
+        expect(res.body.email).toBe(users[0].email)
+      })
+      .end(done)
+  })
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({})
+      })
+      .end(done)
+  })
+})
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    var email = 'example@example.com'
+    var password = '123bmd!'
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeDefined()
+        expect(res.body._id).toBeDefined()
+        expect(res.body.email).toBe(email)
+      })
+      .end((err) => {
+        if(err){
+          return done(err)
+        }
+
+        User.findOne({email}).then(user => {
+          expect(user).toBeDefined()
+          expect(user.password).not.toBe(password)
+          done()
+        })
+      })
+  })
+
+  it('should return validation if request is invalid', (done) => {
+    var invalidEmail = "notAnEmail"
+    var password = '123bmd!'
+    request(app)
+      .post('/users')
+      .send(invalidEmail,password)
+      .expect(400)
+      .end(done)
+  })
+
+  it('should not create user if email in use', (done) => {
+    var takenEmail = users[0].email
+    var password = '123bmd!'
+    request(app)
+      .post('/users')
+      .send(takenEmail,password)
+      .expect(400)
+      .end((err) => {
+        if(err){
+          return done(err)
+        }
+
+        User.count().then(count => {
+          expect(count).toBe(2)
+          done()
+        })
+      })
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//.
